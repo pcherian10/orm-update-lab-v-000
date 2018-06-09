b84185b17557f6cbab8c5e3981ab75c3e619b5e9
Paul <pcherian10@gmail.com>
2018-06-09 19:16:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,12 +50,14 @@ class Student
 
 
   def self.find_by_name (name)
+
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
       LIMIT 1
     SQL
+
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
@@ -74,7 +76,7 @@ class Student
 
 
     SQL
-    
+
 
   end
 
