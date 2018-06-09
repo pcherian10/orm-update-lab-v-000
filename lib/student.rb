ac1e944ef5e9501c22a5ebe549ec20e03a6222a1
Paul <pcherian10@gmail.com>
2018-06-09 19:41:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,13 +30,14 @@ class Student
   def save
     if self.id
       self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?,?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.new_from_db (row)
