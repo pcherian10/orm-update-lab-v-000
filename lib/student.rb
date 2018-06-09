573ab8352e58c00d141e568b2e23ee99910de44e
Paul <pcherian10@gmail.com>
2018-06-09 19:43:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
       SET name = ?, grade = ? WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, self.id, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   def self.all
